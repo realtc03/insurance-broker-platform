d32650b64adaeac07b28c894a2314227b80dc600
realtc03 <tryanchristos@gmail.com>
2025-11-13 18:01:21 +0800

    Update Tailwind CSS content paths in config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
