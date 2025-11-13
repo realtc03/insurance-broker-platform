b44c285d8355764e1d4e6836562ce8e871144ab5
realtc03 <tryanchristos@gmail.com>
2025-11-13 17:50:37 +0800

    Update policy-types.controller.ts

--- api/src/policy-types.controller.ts
+++ api/src/policy-types.controller.ts
@@ -1,1 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { SupabaseService } from './supabase.service';
 
+@Controller('policy-types')
+export class PolicyTypesController {
+  constructor(private readonly supabase: SupabaseService) {}
+
+  @Get()
+  async findAll() {
+    return this.supabase.getPolicyTypes();
+  }
+}
